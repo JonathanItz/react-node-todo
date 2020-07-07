b78dedc1acb6692f6715c501b5b94000f4e96c0d
Jonathan Itzen <jonathanitzen95@gmail.com>
2020-07-07 18:54:51 -0500

    init todo app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './sass/App.scss';
+
+class App extends React.Component {
+  render() {
+    return <div>This will be the container component of the app</div>
+  }
+}
+
+export default App;
